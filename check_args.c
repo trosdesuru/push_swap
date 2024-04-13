f3e18ec84e43ef73696ee170d02bcbdbe8c819dc
Edu <126463577+trosdesuru@users.noreply.github.com>
2024-04-13 02:29:02 +0200

    version 1.1 | push_swap

--- check_args.c
+++ check_args.c
@@ -1,61 +1,14 @@
 
 int check_args(int argc, char **argv)
 {
-    if (!check_argc(argc, argv) || !check_argv(argv))
-    {
-        perror_printargv();
-        return (-1);
-    }
-    return (0);
-}
-
-int check_argc(int argc, char **argv)
-{
     if (argc <= 1)
     {
         perror_printargc();
-        return(-1);
+        return(0);
     }
-    if (argc >= 2)
+    else if (argc >= 2)
         check_argv(argv);
-    return (0);
-}
-
-check_isdigit(char **argv)
-{
-    int i;
-    int j;
-
-    i = 1;
-    j = 0;
-    while (argv[i][j] != '\0')
-    {
-        if (argv[i][j] == '-' || argv[i][j] == '+')
-        if (argv[i][j] == ' ' || argv[i][j] == '\t')
-        if (argv[i][j] >= '0' && argv[i][j] <= '9')
-        j++;
-    }
-    i++;
-    return (0);
-}
-
-check_ischar(char **argv)
-{
-    int i;
-    int j;
-
-    i = 1;
-    j = 0;
-    while (argv[i][j])
-    {
-        if (argv[i][j] <= '0' && argv[i][j] >= '9')
-        {
-            perror_printchar();
-            return (-1);
-        }
-        j++;
-    }
-    i++;
+    return (1);
 }
 
 int check_argv(char **argv)
@@ -69,15 +22,94 @@ int check_argv(char **argv)
         j = 0;
         //write(1, &argv[i][j], 1);
         if (check_isdigit(argv))
-            return (0);
+            return (1);
         else if (check_ischar(argv))
         {
             perror_printer();
-            return (-1);
+            return (0);
         }
         j++;
     }
     i++;
-    return (0);
+    return (1);
 }
 
+/*FIX CHECK IS DIGIT FUNCTION
+
+check_isdigit(char **argv)
+{
+    int i;
+    int j;
+
+    i = 1;
+    j = 0;
+    while (argv[i][j])
+    {
+
+        if (argv[i][j] == '-' || argv[i][j] == '+')
+        {
+            //write(1, &argv[i[j], 1]);
+        }
+
+        if (argv[i][j] == ' ' || argv[i][j] == '\t')
+        {
+
+        }
+
+        if (argv[i][j] >= '0' && argv[i][j] <= '9')
+        {
+
+        }
+
+        else if (check_dup(argv))
+        {
+            perror_printdup();
+            return (0);
+        }
+        j++;
+    }
+    i++;
+    return (1);
+}*/
+
+check_ischar(char **argv)
+{
+    int i;
+    int j;
+
+    i = 1;
+    j = 0;
+    while (argv[i][j])
+    {
+        if (argv[i][j] <= '0' && argv[i][j] >= '9')
+        {
+            perror_printchar();
+            return (0);
+        }
+        j++;
+    }
+    i++;
+}
+/* FIX CHECK DUPLICATE FUNCTION
+
+int check_dup(int argc, char **argv)
+{
+    int     i;
+    int     j;
+    size_t  n;
+    
+    i = 1;
+    j = 0;
+    while (argv[i][j])
+    {
+        n = 0;
+        argv[i][n] = argv[i][j];
+        if (nbr == argv[i][j])
+        {
+            perror_printer();
+            return (0);
+        }
+        j++;
+    }
+    n++;
+}*/
